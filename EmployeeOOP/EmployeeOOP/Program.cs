4ce7da3bdab382bdabe7dd4bfe8c0fafec0440bd
DIEGO ALEXANDER VALENCIA CALDER�N <diegovalencia313667@correo.itm.edu.co>
2023-09-16 18:04:03 -0500

    Date.cs Class created

--- EmployeeOOP/EmployeeOOP/Program.cs
+++ EmployeeOOP/EmployeeOOP/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EmployeeOOP.Classes;
+
+Console.WriteLine("OOP APPLICATION");
+Console.WriteLine("---------------");
+
+//Inicialización de clase Date
+Date dateObject = new Date(4,5,2003);               //Parametrización de datos, Día/mes/Año
+Console.WriteLine(dateObject.ToString());           //
